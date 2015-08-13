bbefbcfd6f1a1e87fcf0eaa1100ec6922a5eae4f
sxh沈新华 <shen_xh@ctrip.com>
2015-08-13 11:21:07 +0800

    新增Class

---
+++ SmartWinControls/Common/SmartControlType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartWinControls.Common
+{
+    /// <summary>
+    /// @Author:Robin
+    /// @Date:2015-08-07
+    /// @Desc：控件类型
+    /// </summary>
+    public enum SmartControlType
+    {
+        Button,
+        RadioButton,
+        CheckBox,
+        ComboBox,
+        ListBox,
+        Label
+    }
+}
